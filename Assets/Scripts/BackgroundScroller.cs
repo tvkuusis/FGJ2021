83306866e9636bc5de0add252a2312be069de8b4
Tumppi <kuusisto.tuomo@gmail.com>
2021-01-30 19:24:49 +0200

    Background scroller

--- Assets/Scripts/BackgroundScroller.cs
+++ Assets/Scripts/BackgroundScroller.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class BackgroundScroller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform background1;
+    public Transform background2;
+    public float speed = 10f;
+    public float moveAmount = 4870;
+
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(background1.transform.localPosition.y < -1800)
+        {
+            background1.transform.localPosition = new Vector3(background1.transform.position.x, background1.transform.position.y + moveAmount, background1.transform.position.z);
+        }
+
+        if(background2.transform.localPosition.y < -1800)
+        {
+            background2.transform.localPosition = new Vector3(background2.transform.position.x, background2.transform.position.y + moveAmount, background2.transform.position.z);
+        }
+
+        background1.Translate(-Vector2.up * speed, Space.World);
+        background2.Translate(-Vector2.up * speed, Space.World);
     }
 }
